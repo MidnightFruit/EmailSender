de1ba08fb5015744a67c2d7fbe077e167227f84c
MidnightFruit <81094799+MidnightFruit@users.noreply.github.com>
2024-09-11 22:11:33 +0700

    Исправление + не забытые доработки

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-from blog.views import BlogPostCreateView, BlogPostListView, BlogPostDetailView, BlogPostUpdateView, BlogPostDeleteView
+from blog.views import BlogPostListView
 from blog.apps import BlogConfig
 from django.urls import path
 
@@ -6,8 +6,5 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path('blog/', BlogPostListView.as_view(), name='blog'),
-    path('create/', BlogPostCreateView.as_view(), name='create_post'),
-    path('post/<int:pk>/', BlogPostDetailView.as_view(), name='read_post'),
-    path('update/<int:pk>/', BlogPostUpdateView.as_view(), name='update_post'),
-    path('delite/<int:pk>/', BlogPostDeleteView.as_view(), name='delete_post'),
+
 ]
